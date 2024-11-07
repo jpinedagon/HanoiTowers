6f134400c673f2e3d14c3eadb4cec3e13d4a2330
Javier Pineda <jpgpineda98@gmail.com>
2024-11-07 13:10:35 -0600

    adding UI elements and binding them to the view controller

--- TowerOfHanoi/Presentation/ViewController.swift
+++ TowerOfHanoi/Presentation/ViewController.swift
@@ -8,12 +8,36 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var discNumberTextField: UITextField!
+    @IBOutlet weak var firstStackView: UIStackView!
+    @IBOutlet weak var secondStackView: UIStackView!
+    @IBOutlet weak var thirdStackView: UIStackView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        setupView()
+    }
+    
+    private func setupView() {
+        let toolBar = UIToolbar(frame: CGRect(origin: .zero, size: .init(width: UIScreen.main.bounds.width, height: 30)))
+        
+        let flexSpace = UIBarButtonItem(barButtonSystemItem: .flexibleSpace, target: nil, action: nil)
+        let doneBtn = UIBarButtonItem(title: "Set", style: .done, target: self, action: #selector(handleSetButtonOnTextfield))
+        
+        toolBar.setItems([flexSpace, doneBtn], animated: false)
+        toolBar.sizeToFit()
+        
+        discNumberTextField.inputAccessoryView = toolBar
+    }
+    
+    @objc private func handleSetButtonOnTextfield() {
+        guard let text = discNumberTextField.text,
+              !text.isEmpty else { return }
+        view.endEditing(true)
     }
 
-
+    @IBAction func solveTower(_ sender: UIButton) {
+        
+    }
 }
-
